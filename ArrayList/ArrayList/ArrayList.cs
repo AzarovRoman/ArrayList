450ce66db568eee34a3feea7dff83230eaaa50af
AzarovRoman <azarovrom9215@gmail.com>
2021-11-02 22:49:52 +0300

    add some methods
    Add
    Add To Beginning
    expand ArrayList
    WriteToConsole

---
+++ ArrayList/ArrayList/ArrayList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Lists
+{
+    public class ArrayList
+    {
+        public int Length { get; private set; } //длинна с точки зрения юзера
+
+        private const int minArrayLength = 10;
+
+        private int[] _array; //подкапотный массив
+
+        public ArrayList()
+        {
+            Length = 0;
+            _array = new int[minArrayLength];
+        }
+
+        public void Add(int value)
+        {
+            if (Length == _array.Length)
+            {
+                expandArrayList();
+            }
+            _array[Length] = value;
+            Length++;
+        }
+
+        public void AddToBeginning(int value)
+        {
+            if (Length == _array.Length)
+            {
+                expandArrayList();
+            }
+
+            for (int i = Length-1; i > 0; i--)
+            {
+                _array[i+1] = _array[i];
+            }
+            Length++;
+            _array[0] = value;
+        }
+
+        public void WriteToConsole()
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                Console.Write($"{_array[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        private void expandArrayList()
+        {
+            int[] tmpArray = new int[(int)(Length * 1.5)];
+            for (int i = 0; i < _array.Length; i++)
+            {
+                tmpArray[i] = _array[i];
+            }
+            _array = tmpArray;
+        }
+    }
+}
---
+++ ArrayList/Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using Lists;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+
+            al.Add(1);
+            al.Add(2);
+            al.Add(3);
+            al.Add(4);
+            al.Add(5);
+            al.Add(1);
+            al.Add(2);
+            al.Add(3);
+            al.Add(4);
+            al.Add(2);
+            al.Add(3);
+            al.Add(4);
+            al.Add(5);
+
+            al.WriteToConsole();
+
+            al.AddToBeginning(2);
+            al.Add(2);
+            al.WriteToConsole();
+
+
+        }
+    }
+}

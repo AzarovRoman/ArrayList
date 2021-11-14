3fd3e6f9a0f7ea8029f7670e8ddc3bfc5fd38054
AzarovRoman <azarovrom9215@gmail.com>
2021-11-15 02:12:30 +0300

    start DoubleLinkedList

---
+++ ArrayList/DoubleLinkedList/DoubleLinked.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace DoubleLinkedList
+{
+    public class DoubleLinked
+    {
+        private Node _root;
+
+        public DoubleLinked()
+        {
+            _root = null;
+        }
+
+        public DoubleLinked(int value)
+        {
+            _root = new Node(value);
+        }
+
+        public DoubleLinked(int[] array)
+        {
+            if (array.Length == 0)
+            {
+                _root = null;
+            }
+            else
+            {
+                Node node = new Node(array[0]);
+                _root = node;
+                for (int i = 1; i < array.Length; i++)
+                {
+                    node.Next = new Node(array[i]);
+                    node = node.Next;
+                }
+            }
+        }
+
+        public void Add()
+        {
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            DoubleLinked linkedList = (DoubleLinked)obj;
+
+            Node tmpRoot = _root;
+            Node tmp = linkedList._root;
+
+            if (tmpRoot == null && tmp == null)
+            {
+                return true;
+            }
+
+            while (tmpRoot != null || tmp != null)
+            {
+                if (tmpRoot.Value != tmp.Value)
+                {
+                    return false;
+                }
+                if ((tmpRoot.Next == null && tmp.Next != null)
+                    || (tmpRoot.Next != null && tmp.Next == null))
+                {
+                    return false;
+                }
+
+                tmpRoot = tmpRoot.Next;
+                tmp = tmp.Next;
+            }
+            return true;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            Node tmp = _root;
+            while (tmp != null)
+            {
+                str += $"{tmp.Value} ";
+                tmp = tmp.Next;
+            }
+            return str;
+        }
+    }
+}
---
+++ ArrayList/DoubleLinkedList/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleLinkedList
+{
+    internal class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+        public Node Prev { get; set; }
+        
+        public Node(int value)
+        {
+            Value = value;
+            Next = null;
+            Prev = null;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value} ";
+        }
+    }
+}
